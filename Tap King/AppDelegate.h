0b8f6a82f1367f07362316c0eaead23569326a7f
kevin dhimitri <kevin@kevins-MacBook.local>
2014-07-10 19:54:55 -0400

    Initial Commit

---
+++ Tap King/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Tap King
+//
+//  Created by kevin dhimitri on 7/10/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
